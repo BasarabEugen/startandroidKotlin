97cccd51c608896352ee2e2637fd13e00b4258d7
basarab.eugen@gmail.com <basarab.eugen@gmail.com>
2019-01-09 11:11:02 +0300

    P0051_LayoutFiles

---
+++ module/p0051_layoutfiles/src/main/java/com/example/p0051_layoutfiles/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.p0051_layoutfiles
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.myscreen)
+    }
+}
